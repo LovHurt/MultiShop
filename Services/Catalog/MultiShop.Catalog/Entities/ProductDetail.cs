4e053481f9804b076d4f211b2f67b1fb594cc7fe
Alican Dağıdır <alicandagidir@hotmail.com>
2024-05-02 16:08:55 +0300

    Add project files.

---
+++ Services/Catalog/MultiShop.Catalog/Entities/ProductDetail.cs
@@ -0,0 +1,9 @@
+﻿namespace MultiShop.Catalog.Entities
+{
+    public class ProductDetail
+    {
+        public string ProductDetailId { get; set; }
+        public string ProductDescription { get; set; }
+        public string ProductInfo { get; set; }
+    }
+}
