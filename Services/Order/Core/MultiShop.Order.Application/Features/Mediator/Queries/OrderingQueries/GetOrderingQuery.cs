42f7a085f105f1f7b36e4636dfed485c684bd119
Alican Dağıdır <alicandagidir@hotmail.com>
2024-05-20 19:16:26 +0300

    Mediator Pattern Add for Ordering Entity and Ordering CRUD Operations Add

---
+++ Services/Order/Core/MultiShop.Order.Application/Features/Mediator/Queries/OrderingQueries/GetOrderingQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using MultiShop.Order.Application.Features.Mediator.Results.OrderingResults;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Features.Mediator.Queries.OrderingQueries;
+
+public class GetOrderingQuery:IRequest<List<GetOrderingQueryResult>>
+{
+
+}
--- Services/Order/Presentation/MultiShop.Order.WebApi/Program.cs
+++ Services/Order/Presentation/MultiShop.Order.WebApi/Program.cs
@@ -1,4 +1,25 @@
+using MultiShop.Order.Application.Features.CQRS.Handlers.AddressHandlers;
+using MultiShop.Order.Application.Features.CQRS.Handlers.OrderDetailHandlers;
+using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Application.Services;
+using MultiShop.Order.Persistence.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+builder.Services.AddApplicationService(builder.Configuration);
+
+builder.Services.AddScoped<GetAddressQueryHandler>();
+builder.Services.AddScoped<GetAddressByIdQueryHandler>();
+builder.Services.AddScoped<CreateAddressCommandHandler>();
+builder.Services.AddScoped<RemoveAddressCommandHandler>();
+builder.Services.AddScoped<UpdateAddressCommandHandler>();
+
+builder.Services.AddScoped<GetOrderDetailQueryHandler>();
+builder.Services.AddScoped<GetOrderDetailByIdQueryHandler>();
+builder.Services.AddScoped<CreateOrderDetailCommandHandler>();
+builder.Services.AddScoped<RemoveOrderDetailCommandHandler>();
+builder.Services.AddScoped<UpdateOrderDetailCommandHandler>();
 
 // Add services to the container.
 
